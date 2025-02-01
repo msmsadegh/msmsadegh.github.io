dff84471fea534860d80e786f2178809903a408c
msmsadegh <39742727+msmsadegh@users.noreply.github.com>
2025-02-01 15:47:28 +0000

    Deploying to gh-pages from @ msmsadegh/msmsadegh.github.io@5c4960dc013b570172980e0829036c0c281f4c9a 🚀

---
+++ _pages/cv2.md
@@ -0,0 +1,21 @@
+[//]: # (---)
+
+[//]: # (layout: cv2)
+
+[//]: # (permalink: /cv2/)
+
+[//]: # (title: cv2)
+
+[//]: # (nav: true)
+
+[//]: # (nav_order: 5)
+
+[//]: # (cv_pdf: Mohammad-Sadegh-Maghareh-Work-Resume.pdf # you can also use external links here)
+
+[//]: # (description: This is a description of the page. You can modify it in '_pages/cv.md'. You can also change or remove the top pdf download button.)
+
+[//]: # (toc:)
+
+[//]: # (  sidebar: left)
+
+[//]: # (---)
--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,15 +30,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-academic-cv",
-          title: "academic-cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "nav-industry-cv",
-          title: "industry-cv",
+        },{id: "nav-cv",
+          title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
